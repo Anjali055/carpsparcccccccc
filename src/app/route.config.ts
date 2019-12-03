9afbff9ade26447f9f06415c65245f7c8c3dc806
yogesh bagade <yogeshbagade25@gmail.com>
2019-12-03 11:51:43 +0530

    d

--- src/app/route.config.ts
+++ src/app/route.config.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router'
+import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UserServiceButtonsComponent } from './user-service-buttons/user-service-buttons.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
